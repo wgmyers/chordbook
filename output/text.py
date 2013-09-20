0422085e9b738ead5772c1f5527df139aea4da8a
Wayne Myers <wgmyers@gmail.com>
2013-09-20 19:42:01 +0100

    Basic functionality implemented

--- output/text.py
+++ output/text.py
@@ -6,30 +6,53 @@ from _base import CbkOutputter
 
 class text(CbkOutputter):
 
+    def make_underline(self, s, char):
+        """Take a string and underline char, return underlined version"""
+        s = s + "\n" +(char * len(s)) + "\n\n"
+        return s
+
+    def make_tune(self, t):
+        """Format an individual tune"""
+        s = t.name
+        s = self.make_underline(s, "-")
+
+        for section in t.structure:
+            s += section.title() + ":\n"
+            s += t.__getattribute__(section) + "\n\n"
+
+        s += "\n"
+
+        return s
+
     def make_contents(self, b):
         """Return a string for the contents of the output"""
         c = b.get_contents()
 
-        s = ""
+        s = self.make_underline("Contents", "-")
         for title in c:
             s += title
             s += "\n"
-
+        s += "\n"
         return s
 
     def make_header(self, b):
         """Return a string for the header of the output"""
         s = b.band + " (version " + b.version + ")"
         s = s.encode('utf-8')
-        s = s + "\n" + ("=" * len(s)) + "\n"
+        s = self.make_underline(s, "=")
         return s
 
     def make_book(self, b):
         """Output plaintext version of book data"""
+        o = ""
+        
         s = self.make_header(b)
-        print s
+        o += s
 
         s = self.make_contents(b)
-        print s
+        o += s
 
-        super(text, self).make_book(b)
+        for t in b.tunes:
+            o += self.make_tune(t)
+
+        print o
